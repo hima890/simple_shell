29c139f8cd6f138bcad66ef73196242f58680024
Ahmed Basher <basherahmed362@gmial.com>
2023-11-12 21:01:04 +0400

    update

---
+++ build/print_prompt.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_prompt - Prints a command prompt, represented 
+ * by the '$' symbol, to the standard output (stdout).
+ * 
+ * The prompt is displayed only if the program is being 
+ * run in an interactive terminal environment.
+ * 
+ * It checks whether the standard input (stdin) is 
+ * connected to a terminal using the isatty function.
+ * 
+ * Return: nothing function is of type void.
+*/
+void print_prompt()
+{
+    if (isatty(STDIN_FILENO))
+    {
+        /* code */
+        printf("$ ");
+    }
+
+}
