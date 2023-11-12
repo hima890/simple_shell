7363a5daeff8a929a6411d082fb19052da330811
Ahmed Basher <basherahmed362@gmial.com>
2023-11-12 21:07:15 +0400

    update

---
+++ Mido_Testing/build/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <limits.h>
+
+int command_exists(char *cmd_name);
+void exit_command(char **cmd_argv, char *line_buffer);
+void handle_exit(char **cmd_argv, char *line_buffer);
+void tokenize_input(char *line_buffer, char **cmd_argv, int *argc, int max);
+char* find_command_path(const char *cmd_name, char *absolute_path, const char *env_path);
+void handle_env(char **env);
+void print_prompt();
+
+#endif /* MAIN_H */
