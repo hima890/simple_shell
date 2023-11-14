da97580fdbf1b7501855f62f413d5cf8f3656062
hima890 <hfibrahim90@gmail.com>
2023-11-14 15:22:04 +0200

    All work for shell exit and exit advance

--- build/exit_command.c
+++ build/exit_command.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
+
+int isNumeric(const char *str)
+{
+    char *endptr;
+
+    strtol(str, &endptr, 10);
+    return (*endptr == '\0');
+}
+
+
 /**
  * exit_command - Execute a command, handling special cases.
  *
@@ -20,24 +31,33 @@
  * Example:
  *   Execute 'exit' command with custom status: `exit 42`
  */
-void exit_command(char **cmd_argv, char *line_buffer, int error, int arc)
+void exit_command(char **cmd_argv, char *line_buffer, int error, int arc, char *const *argv)
 {
-	if (strcmp(cmd_argv[0], "exit") == 0)
-	{
-		int exit_status = 0;
-		int i;
+    int exit_status = 0;
+    int i;
 
-		if (cmd_argv[1] != NULL)
-		{
-			exit_status = atoi(cmd_argv[1]);
-			free(line_buffer);
-			for (i = 0; i < arc; i++)
-			{
-				free(cmd_argv[i]);
-			}
-			exit(exit_status);
-		}
-		else if (error == 2)
+    if (strcmp(cmd_argv[0], "exit") == 0)
+    {
+        if (cmd_argv[1] != NULL)
+        {
+            if (!isNumeric(cmd_argv[1]))
+            {
+                fprintf(stderr, "%s: %d: exit: Illegal number: %s\n", argv[0], 1, cmd_argv[1]);
+                exit_status = 2;
+            }
+            else
+            {
+                exit_status = atoi(cmd_argv[1]);
+            }
+
+            if (exit_status < 0)
+            {
+                fprintf(stderr, "%s: %d: exit: Illegal number: %d\n", argv[0], 1, exit_status);
+                exit_status = 2;
+            }
+        }
+
+		if (error == 2)
 		{
 			free(line_buffer);
 			for (i = 0; i < arc; i++)
@@ -46,12 +66,13 @@ void exit_command(char **cmd_argv, char *line_buffer, int error, int arc)
 			}
 			exit(error);
 		}
-		free(line_buffer);
-		for (i = 0; i < arc; i++)
-		{
-			free(cmd_argv[i]);
-		}
-		exit(exit_status);
 
-	}
+        free(line_buffer);
+        for (i = 0; i < arc; i++)
+        {
+            free(cmd_argv[i]);
+        }
+
+        exit(exit_status);
+    }
 }
--- build/main.h
+++ build/main.h
@@ -13,7 +13,8 @@
 #define MAX_ARGS 16000
 
 int command_exists(char *cmd_name);
-void exit_command(char **cmd_argv, char *line_buffer, int error, int arc);
+void exit_command(char **cmd_argv, char *line_buffer, int error, int arc, char *const *argv);
+int isNumeric(const char *str);
 void handle_exit(char **cmd_argv, char *line_buffer);
 void tokenize_input(char *line_buffer, char **cmd_argv, int *argc, int max);
 char *f_path(char *cmd, char *abs, int is, int *er, char *const *argv, char **env);
